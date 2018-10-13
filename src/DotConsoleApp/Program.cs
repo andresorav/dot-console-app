f313269a69b6b38e7b6cc5e0045177ca6bdc5a13
Andres Orav <andres.orav@pipedrive.com>
2018-10-13 14:08:05 +0300

    Initial commit

---
+++ src/DotConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+
+namespace DotConsoleApp
+{
+    internal static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var host = new HostBuilder()
+                .Build();
+
+            await host.RunAsync();
+        }
+    }
+}
